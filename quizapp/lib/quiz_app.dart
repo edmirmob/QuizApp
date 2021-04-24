f0a309e23c0e41968c20b64b5a3727dfddaffc5f
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-04-24 14:24:41 +0200

    initial

---
+++ quizapp/lib/quiz_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuizApp extends StatefulWidget {
+  @override
+  _QuizAppState createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
